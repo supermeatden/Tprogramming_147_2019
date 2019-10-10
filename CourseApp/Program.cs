aeb27bc23b08cdf09d3f75540aef1d1c06e6c7f9
Eugeny Konstantinov <eugeny.konstantinov@rp.ru>
2019-10-10 09:39:40 +0300

    Sample of 1st lab

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -4,9 +4,61 @@ namespace CourseApp
 {
     public class Program
     {
+        public static double MyFunction(double a, double b, double x)
+        {
+            var c = (b * x) + a;
+            return c;
+        }
+
+        public static double[] TaskA (
+                                     double a,
+                                     double b,
+                                     double xn,
+                                     double xk,
+                                     double dx)
+        {
+            var steps = (int)Math.Floor((xk - xn) / dx);
+            var y = new double[steps];
+            var i = 0;
+            for (var x = xn; x < xk; x += dx)
+            {
+                y[i] = MyFunction(a, b, x);
+                i++;
+            }
+
+            return y;
+        }
+
+        public static double[] TaskB (
+                                 double a,
+                                 double b,
+                                 double[] x)
+        {
+            var y = new double[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                y[i] = MyFunction(a, b, x[i]);
+            }
+
+            return y;
+        }
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var taskA = TaskA(2, 3, 0, 5, 1);
+            for (var i = 0; i < taskA.Length; i++)
+            {
+                Console.WriteLine($"y={taskA[i]}");
+            }
+
+            var xB = new double[] { 0, 1, 2, 3 };
+            var taskB = TaskB(2, 3, xB);
+            for (var i = 0; i < xB.Length; i++)
+            {
+                Console.WriteLine($"x={xB[i]} y={taskB[i]}");
+            }
+
             Console.ReadLine();
         }
     }
